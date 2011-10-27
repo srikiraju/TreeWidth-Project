7207fb45ece5c2957d0e5a563982fe1f97335bea
Srikanth Raju <srikiraju@gmail.com>
2011-10-27 01:33:56 -0600

    Implement Basic Tree Width finding
    This is a stupid, try everything variation.

--- src/Main.java
+++ src/Main.java
@@ -8,34 +8,37 @@
  *
  * @author srikanthraju
  */
-import org.jgraph.graph.DefaultEdge;
+import com.treewidth.TreeWidth;
+import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.UndirectedGraph;
 import org.jgrapht.graph.SimpleGraph;
 
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
-        UndirectedGraph<String, DefaultEdge> g =
-            new SimpleGraph<String, DefaultEdge>(DefaultEdge.class);
-
-        String v1 = "v1";
-        String v2 = "v2";
-        String v3 = "v3";
-        String v4 = "v4";
+        SimpleGraph<Integer, DefaultEdge> g =
+            new SimpleGraph<Integer, DefaultEdge>(DefaultEdge.class);
 
         // add the vertices
-        g.addVertex(v1);
-        g.addVertex(v2);
-        g.addVertex(v3);
-        g.addVertex(v4);
+        g.addVertex(1);
+        g.addVertex(2);
+        g.addVertex(3);
+        g.addVertex(4);
+        g.addVertex(5);
+
 
         // add edges to create a circuit
-        g.addEdge(v1, v2);
-        g.addEdge(v2, v3);
-        g.addEdge(v3, v4);
-        g.addEdge(v4, v1);
+        g.addEdge(1, 2);
+        g.addEdge(2, 3);
+        g.addEdge(3, 4);
+        g.addEdge(4, 5);
+        g.addEdge(5, 1);
+
+
         
+        TreeWidth tw = new TreeWidth( g );
+        System.out.println( tw.find() );
+
         System.out.println( g.toString() );
     }
 }
---
+++ src/com/treewidth/TreeWidth.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.treewidth;
+
+import org.jgrapht.Graph;
+import com.treewidth.util.Permute;
+import org.jgrapht.alg.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+
+/**
+ *
+ * @author srikanthraju
+ */
+public class TreeWidth {
+    SimpleGraph<Integer, DefaultEdge> graph;
+    
+    public TreeWidth( SimpleGraph<Integer, DefaultEdge> graph )
+    {
+        this.graph = graph;
+    }
+    public int find()
+    {
+        int tw = 100;
+        Permute permuter = new Permute( graph.vertexSet().toArray() );
+        while( permuter.hasNext() )
+        {
+            Object[] permutation = (Object[])permuter.next();
+            int k = findmaxQ( permutation );
+            if( k < tw )
+                tw = k;
+        }
+        return tw;
+    }
+    
+    private int findmaxQ( Object[] vertices )
+    {
+        int maxq = 0;
+
+        for( int i = 0; i < vertices.length; i++)
+        {
+            int v = (Integer)vertices[i];
+            int Q = 0;
+            //Find Q
+            for( int j = i + 1; j < vertices.length; j++ )
+            {
+                int w = (Integer)vertices[j];
+                SimpleGraph check = (SimpleGraph)graph.clone();
+                for( int tmp = i + 1; tmp < vertices.length; tmp ++ )
+                {
+                    if( (Integer)vertices[tmp] != w )
+                        check.removeVertex( (Integer)vertices[tmp] );
+                }
+                if( DijkstraShortestPath.findPathBetween(check, v, w) != null )
+                    Q++;
+
+            }
+            if( Q > maxq)
+                maxq = Q;
+            
+        }
+        
+        return maxq;
+    }
+}
